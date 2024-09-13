8aabd5311499ad3c46b9d0fb696095c916d12c4d
idwang <vbird.ggg@gmail.com>
2024-09-13 10:45:56 +0800

    feat: using tailwind to implement a banner

--- components/carousel/index.tsx
+++ components/carousel/index.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useCallback, useEffect, useState } from 'react'
+import { ReactNode, useCallback, useEffect, useState } from 'react'
 import Image from 'next/image'
 import useEmblaCarousel from 'embla-carousel-react'
 import { EmblaCarouselType } from 'embla-carousel'
@@ -17,6 +17,20 @@ const sourceList = [
       { text: '专为你的 iPhone 16 打造' },
     ],
     id: 'No1',
+    banner: () => {
+      return (
+        <BannerContainer>
+          <div className="relative bg-[#F6E163] text-[26px] text-[#2c5dab] font-bold	rounded-[16px] p-[12px] pb-[6px] rounded-bl-[0] rounded-br-[0] w-fit">
+            无畏，更自由
+            <BannerAfter />
+          </div>
+          <div className="px-[12px] py-[6px] bg-[#F6E163] w-[228px] rounded-[16px] rounded-tl-[0] text-[14px] text-black">
+            <div>兼具极致防护与个性风格</div>
+            <div>专为你的 iPhone 16 打造</div>
+          </div>
+        </BannerContainer>
+      )
+    },
   },
   {
     image: '/first/2.jpg',
@@ -24,6 +38,17 @@ const sourceList = [
     title: '全新 iPhone 16 手机壳',
     subTitle: ['打造你的个性风格'],
     id: 'No2',
+    banner: () => (
+      <BannerContainer>
+        <div className="relative bg-[#2c5dab] text-[26px] text-[#f6e163] font-bold	rounded-[16px] p-[12px] pb-[6px] rounded-bl-[0] w-fit">
+          全新 iPhone 16 手机壳
+        </div>
+        <div className="relative px-[12px] py-[6px] bg-[#2c5dab] rounded-[16px] rounded-tl-[0] rounded-tr-[0] text-[14px] text-white w-fit">
+          <BannerAfter className="top-0 shadow-[-6px_-6px_1px_#2c5dab] rounded-none rounded-tl-[16px]" />
+          <div>打造你的个性风格</div>
+        </div>
+      </BannerContainer>
+    ),
   },
   {
     image: '/first/3.png',
@@ -31,6 +56,17 @@ const sourceList = [
     title: '丰富的印花设计',
     subTitle: ['支持全新 iPhone 16 系列'],
     id: 'No3',
+    banner: () => (
+      <BannerContainer>
+        <div className="relative bg-[#21a664] text-[26px] text-[#f6e163] font-bold	rounded-[16px] p-[12px] pb-[6px] rounded-bl-[0] w-fit">
+          丰富的印花设计
+        </div>
+        <div className="relative px-[12px] py-[6px] bg-[#21a664] rounded-[16px] rounded-tl-[0] rounded-tr-[0] text-[14px] text-black w-fit">
+          <BannerAfter className="top-0 shadow-[-6px_-6px_1px_#21a664] rounded-none rounded-tl-[16px]" />
+          <div>支持全新 iPhone 16 系列</div>
+        </div>
+      </BannerContainer>
+    ),
   },
   {
     image: '/first/4.png',
@@ -38,6 +74,17 @@ const sourceList = [
     title: '全新 Apple Watch 10 表带',
     subTitle: ['Bounce Odyssey™ 极强一体式表带'],
     id: 'No4',
+    banner: () => (
+      <BannerContainer>
+        <div className="relative bg-[#f15b41] text-[26px] text-[#f6e163] font-bold	rounded-[16px] p-[12px] pb-[6px] rounded-bl-[0] w-fit">
+          全新 Apple Watch 10 表带
+        </div>
+        <div className="relative px-[12px] py-[6px] bg-[#f15b41] rounded-[16px] rounded-tl-[0] rounded-tr-[0] text-[14px] text-black w-fit">
+          <BannerAfter className="top-0 shadow-[-6px_-6px_1px_#f15b41] rounded-none rounded-tl-[16px]" />
+          <div>Bounce Odyssey™ 极强一体式表带</div>
+        </div>
+      </BannerContainer>
+    ),
   },
   {
     image: '/first/5.png',
@@ -45,6 +92,20 @@ const sourceList = [
     title: 'MagSafe 配件',
     subTitle: ['Snappy™ 系列产品', '为你的 iPhone 16 创造更多可能'],
     id: 'No5',
+    banner: () => {
+      return (
+        <BannerContainer>
+          <div className="relative bg-[#fecad6] text-[26px] text-[#2c5dab] font-bold	rounded-[16px] p-[12px] pb-[6px] rounded-bl-[0] rounded-br-[0] w-fit">
+            MagSafe 配件
+            <BannerAfter className="shadow-[-6px_6px_1px_#fecad6] " />
+          </div>
+          <div className="px-[12px] py-[6px] bg-[#fecad6] w-[269px] rounded-[16px] rounded-tl-[0] text-[14px] text-black">
+            <div>Snappy™ 系列产品</div>
+            <div>为你的 iPhone 16 创造更多可能</div>
+          </div>
+        </BannerContainer>
+      )
+    },
   },
   {
     image: '/first/6.mp4',
@@ -53,39 +114,33 @@ const sourceList = [
     title: '屏幕保护贴',
     subTitle: ['提供多方位防护'],
     type: 'mp4',
+    banner: () => (
+      <BannerContainer>
+        <div className="relative bg-[#F6E163] text-[26px] text-[#2c5dab] font-bold rounded-[16px] p-[12px] pb-[6px] rounded-bl-[0] w-fit">
+          屏幕保护贴
+        </div>
+        <div className="relative px-[12px] py-[6px] bg-[#F6E163] rounded-[16px] rounded-tl-[0] rounded-tr-[0] text-[14px] text-black w-fit">
+          <BannerAfter className="top-0 shadow-[-6px_-6px_1px_#F6E163] rounded-none rounded-tl-[16px]" />
+          <div>提供多方位防护</div>
+        </div>
+      </BannerContainer>
+    ),
   },
 ]
 
-function CarouselBanner({
-  title,
-  subTitle,
-  id,
-}: {
-  id: string
-  title: string
-  subTitle: (string | { text: string })[]
-}) {
-  // todo font-family
-  // todo replace embla-carousel-react
-
+function BannerContainer({ children }: { children: ReactNode }) {
   return (
-    <div className={carouselCss.bannerContainer} data-id={id}>
-      <div className={`${carouselCss.bannerTitle} ${carouselCss.curveCorner}`}>
-        {title}
-      </div>
-
-      <div
-        className={`${carouselCss.bannerSubtitle} ${carouselCss.curveCorner}`}
-      >
-        {subTitle.map(item => {
-          if (typeof item === 'string') {
-            return <div key={item}>{item}</div>
-          }
-
-          return <div key={item.text}>{item?.text}</div>
-        })}
-      </div>
+    <div className="absolute bottom-[42px] left-[16px] leading-[1.2]">
+      {children}
     </div>
+  )
+}
+
+function BannerAfter({ className }: { className?: string }) {
+  return (
+    <div
+      className={`absolute bottom-0 w-[18px] h-[23px] rounded-[16px] right-[-18px] shadow-[-6px_6px_1px_#F6E163] ${className}`}
+    />
   )
 }
 
@@ -170,15 +225,13 @@ export default function Carousel() {
               )
           }
 
+          const Banner = item.banner
+
           return (
             <div key={item.id} className={carouselCss.slideContainer}>
               {slide}
 
-              <CarouselBanner
-                id={item.id}
-                title={item.title}
-                subTitle={item.subTitle}
-              />
+              <Banner />
             </div>
           )
         })}
