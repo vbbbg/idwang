14d1fc358642e8a7fc1c490beecd6202b5107dd4
idwang <vbird.ggg@gmail.com>
2024-12-26 15:33:47 +0800

    feat: 浏览器tab拖动动画

---
+++ app/feature/constant/index.ts
@@ -0,0 +1,28 @@
+export type ITab = { key: string; label: string }
+
+export const Tabs: ITab[] = [
+  {
+    key: '/home',
+    label: '新标签页',
+  },
+  {
+    key: '/orders',
+    label: '新标签页2',
+  },
+  {
+    key: '/products',
+    label: '新标签页3',
+  },
+  {
+    key: '/coupons',
+    label: '新标签页4',
+  },
+  {
+    key: '/address',
+    label: '新标签页5',
+  },
+  {
+    key: '/payment',
+    label: '新标签页6',
+  },
+]
